2a116318716c203f47d243ef61b76644e3204fe4
Willy Ovalle <willyovalle16@gmail.com>
2016-10-23 23:48:53 -0400

    Enhancement: tooling

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.json({status: 'online'});
+    res.json({ status: 'online' });
 });
 
 module.exports = router;
